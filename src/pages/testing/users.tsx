d90e01f1608b1fdc69c0bfaeebb368c745de0746
CarlJMcGee <98133059+CarlJMcGee@users.noreply.github.com>
2022-09-13 17:46:57 -0500

    user testing complete

--- src/pages/testing/users.tsx
+++ src/pages/testing/users.tsx
@@ -1,26 +1,59 @@
+import { NextPage } from "next";
 import Head from "next/head";
 import * as React from "react";
+import { userRouter } from "../../server/router/users";
 import { trpc } from "../../utils/trpc";
 
 export interface IUserTestingProps {}
 
-export default function UserTesting(props: IUserTestingProps) {
+const UserTesting: NextPage = (props: IUserTestingProps) => {
   // state
   const [userSearch, setUserSearch] = React.useState("");
-  const [userData, setUser] = React.useState();
+  const [newUserInfo, setNewUserInfo] = React.useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [passToCheck, setPassToCheck] = React.useState({ email: "", pass: "" });
 
+  // query
   const { data: users, isLoading: gettingUsers } = trpc.useQuery([
     "user.getAll",
   ]);
+
+  // mutations
   const {
     mutateAsync: findUser,
     isLoading: findingUser,
     data: user,
   } = trpc.useMutation(["user.getOne"]);
+  const {
+    mutateAsync: addUser,
+    isLoading: addingUser,
+    data: newUser,
+  } = trpc.useMutation(["user.addUser"]);
+  const { mutateAsync: checkPass, data: isPassword } = trpc.useMutation([
+    "user.loginTemp",
+  ]);
 
+  // handlers
   const searchHandler = async function (e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
     await findUser({ userId: userSearch });
+  };
+  const addUserHandler = async function (e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    await addUser({
+      username: newUserInfo.username,
+      email: newUserInfo.email,
+      password: newUserInfo.password,
+    });
+  };
+  const passCheckHandler = async function (
+    e: React.FormEvent<HTMLFormElement>
+  ) {
+    e.preventDefault();
+    await checkPass({ email: passToCheck.email, pass: passToCheck.pass });
   };
 
   return (
@@ -47,11 +80,14 @@ export default function UserTesting(props: IUserTestingProps) {
           </ul>
         </>
       )}
+
       <br />
+
       <form onSubmit={searchHandler}>
+        <h3 className="text-3xl">Search</h3>
         <input
           type="text"
-          className="border border-black rounded"
+          className="bg-slate-300 rounded m-2 p-1"
           placeholder="User ID"
           value={userSearch}
           onChange={(e) => setUserSearch(e.target.value)}
@@ -64,9 +100,79 @@ export default function UserTesting(props: IUserTestingProps) {
         <p>
           <span className="font-bold">{user.name}</span>{" "}
           {user.canPost ? "Can post" : "Cannot post"} and{" "}
-          {user.banned ? "is banned" : "is not banned"}
+          {user.banned ? "is banned" : "is not banned"} and has{" "}
+          {user._count.posts} posts and {user._count.comments} comments
         </p>
+      )}
+
+      <br />
+
+      <form onSubmit={addUserHandler}>
+        <h3 className="text-3xl">New User</h3>
+        <input
+          type="text"
+          className="bg-slate-300 rounded m-2 p-1"
+          placeholder="username"
+          value={newUserInfo.username}
+          onChange={(e) =>
+            setNewUserInfo({ ...newUserInfo, username: e.target.value })
+          }
+        />
+        <input
+          type="text"
+          className="bg-slate-300 rounded m-2 p-1"
+          placeholder="email"
+          value={newUserInfo.email}
+          onChange={(e) =>
+            setNewUserInfo({ ...newUserInfo, email: e.target.value })
+          }
+        />
+        <input
+          type="text"
+          className="bg-slate-300 rounded m-2 p-1"
+          placeholder="password"
+          value={newUserInfo.password}
+          onChange={(e) =>
+            setNewUserInfo({ ...newUserInfo, password: e.target.value })
+          }
+        />
+        <button type="submit" className="bg-gray-300 rounded m-2 p-1">
+          Create Account
+        </button>
+      </form>
+      {newUser && <p>New user {newUser.name} created!</p>}
+
+      <br />
+
+      <form onSubmit={passCheckHandler}>
+        <h3 className="text-3xl">Check Password</h3>
+        <input
+          type="text"
+          placeholder="email"
+          className="bg-slate-300 rounded m-2 p-1"
+          onChange={(e) =>
+            setPassToCheck({ ...passToCheck, email: e.target.value })
+          }
+        />
+        <input
+          type="text"
+          placeholder="password"
+          className="bg-slate-300 rounded m-2 p-1"
+          onChange={(e) =>
+            setPassToCheck({ ...passToCheck, pass: e.target.value })
+          }
+        />
+        <button type="submit" className="bg-gray-300 rounded m-2 p-1">
+          Check
+        </button>
+      </form>
+      {isPassword ? (
+        <p className="text-green-600">Correct</p>
+      ) : (
+        <p className="text-red-600">Incorrect</p>
       )}
     </div>
   );
-}
+};
+
+export default UserTesting;
--- src/server/router/users.ts
+++ src/server/router/users.ts
@@ -1,6 +1,7 @@
 import { createRouter } from "./context";
 import { z } from "zod";
 import { TRPCError } from "@trpc/server";
+import bcrypt from "bcrypt";
 
 export const userRouter = createRouter()
   .query("getAll", {
@@ -19,8 +20,16 @@ export const userRouter = createRouter()
     async resolve({ ctx, input }) {
       const User = ctx.prisma.user;
 
-      const user = await User.findFirst({
+      const user = await User.findUnique({
         where: { id: input.userId },
+        include: {
+          _count: {
+            select: {
+              posts: true,
+              comments: true,
+            },
+          },
+        },
       });
 
       if (!user) {
@@ -31,5 +40,46 @@ export const userRouter = createRouter()
       }
 
       return user;
+    },
+  })
+  .mutation("addUser", {
+    input: z.object({
+      username: z.string().trim(),
+      email: z.string().trim(),
+      password: z.string().trim(),
+    }),
+    async resolve({ ctx, input }) {
+      const { email, username, password } = input;
+      const User = ctx.prisma.user;
+
+      const cookedPass = await bcrypt.hash(password, 10);
+
+      const newUser = await User.create({
+        data: {
+          name: username,
+          email: email,
+          password: cookedPass,
+        },
+      });
+
+      return newUser;
+    },
+  })
+  .mutation("loginTemp", {
+    input: z.object({
+      email: z.string().trim(),
+      pass: z.string().trim(),
+    }),
+    async resolve({ ctx, input }) {
+      const user = await ctx.prisma.user.findUnique({
+        where: { email: input.email },
+      });
+      if (user) {
+        return bcrypt.compare(input.pass, user.password);
+      }
+      throw new TRPCError({
+        code: "NOT_FOUND",
+        message: `User not found`,
+      });
     },
   });
