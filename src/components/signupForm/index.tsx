57e2ac8b0a33dc39da5e28d84ce5cd658593d065
CarlJMcGee <98133059+CarlJMcGee@users.noreply.github.com>
2022-10-02 16:03:22 -0500

    added select file and upload button

--- src/components/signupForm/index.tsx
+++ src/components/signupForm/index.tsx
@@ -1,20 +1,28 @@
-import * as React from "react";
+import React, { useState } from "react";
 import { useForm } from "@mantine/form";
-import { Box, Button, Group, TextInput } from "@mantine/core";
+import { Box, Button, FileButton, Group, TextInput, Text } from "@mantine/core";
 import { trpc } from "../../utils/trpc";
 import { signIn } from "next-auth/react";
+import { ImgbbRes } from "../../types/imageUpload";
 
 export default function SignupForm() {
+  // state
+  const [selectedFile, setFile] = useState<File | null>(null);
+
+  // regEx
   const nameVal = /^[a-z0-9$@$!%*?&_]{3,15}$/i;
   const emailVal = /[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}/gim;
   const passVal =
     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&_])[A-Za-z\d$@$!%*?&_]{6,24}$/;
+
+  // form options
   const signupForm = useForm({
     initialValues: {
       username: "",
       email: "",
       password: "",
       passConfirm: "",
+      imageSrc: "",
     },
     validateInputOnBlur: true,
     validate: {
@@ -30,9 +38,12 @@ export default function SignupForm() {
           : `Must be 6-24 characters and contain one uppercase, lowercase, special character, and number`,
       passConfirm: (input, { password }) =>
         password === input ? null : `Passwords do not match`,
+      imageSrc: (input) =>
+        !selectedFile ? `Please upload a profile picture` : null,
     },
   });
 
+  //mutations
   const { mutate: addUser } = trpc.useMutation(["user.addUser"], {
     onSuccess() {
       signIn("credentials", {
@@ -42,13 +53,57 @@ export default function SignupForm() {
     },
   });
 
+  // hanlder
+  const createUserHandler = ({
+    username,
+    email,
+    password,
+    imageSrc,
+  }: typeof signupForm.values) => {
+    if (selectedFile) {
+      const body = new FormData();
+      body.append("image", selectedFile);
+
+      try {
+      } catch (err) {}
+    }
+  };
+  const uploadPicHandler = async (e: React.MouseEvent<HTMLElement>) => {
+    e.preventDefault();
+
+    if (!selectedFile) {
+      return;
+    }
+
+    const body = new FormData();
+    body.append("image", selectedFile);
+
+    try {
+      const req = await fetch(
+        `https://api.imgbb.com/1/upload?&name=${selectedFile.name}&key=dd9e796ad2397d60fca82af89819101b`,
+        {
+          method: "POST",
+          body: body,
+        }
+      );
+      const res: ImgbbRes = await req.json();
+
+      const {
+        data: { image, thumb },
+      } = res;
+      signupForm.setFieldValue("imageSrc", image.url);
+    } catch (err) {
+      if (err) console.error(err);
+    }
+  };
+
+  if (selectedFile) {
+    console.log(selectedFile);
+  }
+
   return (
     <Box>
-      <form
-        onSubmit={signupForm.onSubmit(({ username, email, password }) =>
-          addUser({ username: username, email: email, password: password })
-        )}
-      >
+      <form onSubmit={signupForm.onSubmit(createUserHandler)}>
         <TextInput
           placeholder="Name"
           label="Username"
@@ -76,6 +131,37 @@ export default function SignupForm() {
             withAsterisk
             {...signupForm.getInputProps("passConfirm")}
           />
+        </Group>
+        {selectedFile && (
+          <Text size={"xs"} mt={"sm"}>
+            {selectedFile.name}
+          </Text>
+        )}
+        <Group position="left">
+          <div>
+            <FileButton onChange={setFile} accept="image/png,image/jpeg">
+              {(props) => (
+                <Button
+                  {...props}
+                  color="violet"
+                  className="bg-violet-700"
+                  mt={"md"}
+                >
+                  Select Picture
+                </Button>
+              )}
+            </FileButton>
+          </div>
+          {selectedFile && (
+            <Button
+              color={"violet"}
+              className="bg-purple-500"
+              mt={"md"}
+              onClick={uploadPicHandler}
+            >
+              Upload
+            </Button>
+          )}
         </Group>
 
         <Group position="center" grow={true}>
---
+++ src/types/imageUpload.d.ts
@@ -0,0 +1,28 @@
+export interface ImgbbRes {
+  data: {
+    id: "2ndCYJK";
+    title: "c1f64245afb2";
+    url_viewer: "https://ibb.co/2ndCYJK";
+    url: "https://i.ibb.co/w04Prt6/c1f64245afb2.gif";
+    display_url: "https://i.ibb.co/98W13PY/c1f64245afb2.gif";
+    width: "1";
+    height: "1";
+    size: "42";
+    time: "1552042565";
+    expiration: "0";
+    image: {
+      filename: "c1f64245afb2.gif";
+      name: "c1f64245afb2";
+      mime: "image/gif";
+      extension: "gif";
+      url: "https://i.ibb.co/w04Prt6/c1f64245afb2.gif";
+    };
+    thumb: {
+      filename: "c1f64245afb2.gif";
+      name: "c1f64245afb2";
+      mime: "image/gif";
+      extension: "gif";
+      url: "https://i.ibb.co/2ndCYJK/c1f64245afb2.gif";
+    };
+  };
+}
