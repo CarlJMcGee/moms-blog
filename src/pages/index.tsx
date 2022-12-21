6b43b1744e51e18f61b31f461164fa3014ea548c
CarlJMcGee <98133059+CarlJMcGee@users.noreply.github.com>
2022-12-20 18:45:08 -0600

    fixed post loading skellyton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -47,19 +47,46 @@ const Home: NextPage = () => {
       <>
         <Header sess={sess} />
         <Group position="center" mt={100}>
-          <Paper shadow={"md"} radius={"sm"} p={"md"} withBorder>
-            <Stack>
-              <Skeleton height={10} width={300} mt={6} />
-              <Skeleton height={10} width={150} mt={6} />
-              <Skeleton height={10} width={450} mt={6} />
-              <Skeleton height={10} width={450} mt={6} />
-              <Skeleton height={300} width={450} my={20} />
-              <Skeleton height={10} width={300} mt={6} />
-              <Skeleton height={10} width={300} mt={6} />
-              <Skeleton height={10} width={300} mt={6} />
-              <Skeleton height={10} width={300} mt={6} />
-            </Stack>
-          </Paper>
+          <Stack>
+            <Paper
+              shadow={"md"}
+              radius={"sm"}
+              p={"md"}
+              withBorder
+              className="w-96 m-5"
+            >
+              <Stack>
+                <Skeleton height={10} width={300} mt={6} />
+                <Skeleton height={10} width={150} mt={6} />
+                <Skeleton height={10} width={350} mt={6} />
+                <Skeleton height={10} width={350} mt={6} />
+                <Skeleton height={300} width={350} my={20} />
+                <Skeleton height={10} width={300} mt={6} />
+                <Skeleton height={10} width={200} mt={6} />
+                <Skeleton height={10} width={300} mt={6} />
+                <Skeleton height={10} width={300} mt={6} />
+              </Stack>
+            </Paper>
+            <Paper
+              shadow={"md"}
+              radius={"sm"}
+              p={"md"}
+              withBorder
+              className="w-96 m-5"
+            >
+              <Stack>
+                <Skeleton height={10} width={300} mt={6} />
+                <Skeleton height={10} width={150} mt={6} />
+                <Skeleton height={10} width={350} mt={6} />
+                <Skeleton height={10} width={350} mt={6} />
+                <Skeleton height={300} width={350} my={20} />
+                <Skeleton height={10} width={300} mt={6} />
+                <Skeleton height={10} width={200} mt={6} />
+                <Skeleton height={10} width={300} mt={6} />
+                <Skeleton height={10} width={300} mt={6} />
+              </Stack>
+            </Paper>
+          </Stack>
         </Group>
       </>
     );
